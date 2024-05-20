a394eea72d7398d68977eefc431bd6f83f43238a
gazasolomon <gazasolomon@gmail.com>
2024-05-20 19:44:32 +0000

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,12 @@
 # Define the ulimit command
-$ulimit_command = 'ulimit -n 2048'
 
-# Execute sed command to add ulimit command to ~/.bashrc file
-exec { 'add_ulimit_to_bashrc':
-  command => "/bin/sed -i '/# Add custom configurations/a ${ulimit_command}' /home/holberton/.bashrc",
-  user    => 'holberton',
-  path    => '/bin',
-  onlyif  => "grep -v '^#' /home/holberton/.bashrc | grep -q '${ulimit_command}'",
+# Define the path to the ulimit script
+exec { 'add-user-hard-limit':
+  command => "/bin/sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin',
+}
+# Define the path to the ulimit script
+exec { 'add-user-soft-limit':
+  command => "/bin/sed -i '/^holberton soft/s/4/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin',
 }
