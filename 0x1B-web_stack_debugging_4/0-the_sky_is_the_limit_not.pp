d94eaf3b8505dd869473dd4a9eb10dfaa14e73d4
gazasolomon <gazasolomon@gmail.com>
2024-05-20 18:23:19 +0000

    update to file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,11 @@
-oft ulimit value and hard ulimit value
-$soft_ulimit = 'soft_ulimit_value'
-$hard_ulimit = 'hard_ulimit_value'
-
-# Update the soft ulimit in the nginx configuration file using sed
-exec { 'update_nginx_ulimit':
-  command     => "/bin/sed -i 's/^\(\s*ulimit\s*\).*$/\\1 $hard_ulimit/' /etc/nginx/nginx.conf",
-  path        => '/bin',
-  refreshonly => true,
+# Update the soft ulimit in the nginx default configuration file using sed
+exec { 'update_nginx_default_ulimit':
+  command     => "/bin/sed -i 's/15/4096/' /etc/default/nginx",
+  path        => '/usr/local/bin/:/bin/',
 }
 
 # Restart nginx service if the configuration file has been updated
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Exec['update_nginx_ulimit'],
+service { 'nginx-restart':
+  command  => '/etc/init.d/nginx restart',
+  path     => '/etc/init.d/',
 }
