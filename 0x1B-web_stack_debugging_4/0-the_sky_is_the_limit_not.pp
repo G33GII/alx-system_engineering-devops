0be2557e11344086921cb2961430c874254d0bf9
gazasolomon <gazasolomon@gmail.com>
2024-05-20 18:31:19 +0000

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Update the soft ulimit in the nginx default configuration file using sed
 exec { 'update_nginx_default_ulimit':
-  command     => "/bin/sed -i 's/15/4096/' /etc/default/nginx",
-  path        => '/usr/local/bin/:/bin/',
+  command => "/bin/sed -i 's/15/4096/' /etc/default/nginx",
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Restart nginx service if the configuration file has been updated
 service { 'nginx-restart':
-  command  => '/etc/init.d/nginx restart',
-  path     => '/etc/init.d/',
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
 }
