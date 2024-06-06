74c015559e4ff77ba3160c5f51e426214bbfb3e1
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-06-06 03:44:07 +0000

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# Using strace, find out why Apache is returning a 500 error. Once you find the issue,
+# Using strace, find out why Apache is returning a 500 error.
+# Once you find the issue,
 
 exec {'fix-wordpress':
     command => 'sed -i a/phpp/php/g /var/www/html/wp-settings.php',
