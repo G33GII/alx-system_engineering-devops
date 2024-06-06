4f0bdeacecc0e1e6dc685d8f2a50c2c3ff6a4307
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-06-06 03:41:47 +0000

    chmod

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error. Once you find the issue,
 
-# fix it and then automate it using Puppet
-
-# (instead of using Bash as you were previously doing).
-
-exec {'fix-Apache':
-command => 'sed -i a/phpp/php/g /var/www/html/wp-setting.php'
-path    => '/usr/bin/:/bin'
+exec {'fix-wordpress':
+    command => 'sed -i a/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
