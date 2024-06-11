5ae54fa34ee995d708b2d9b34c9593acd7a0e005
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-06-11 08:24:38 +0000

    minor changes to puppet file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -5,6 +5,7 @@ exec { 'add-user-hard-limit':
   command => "/bin/sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin',
 }
+
 # Define the path to the ulimit script
 exec { 'add-user-soft-limit':
   command => "/bin/sed -i '/^holberton soft/s/4/50000/' /etc/security/limits.conf",
