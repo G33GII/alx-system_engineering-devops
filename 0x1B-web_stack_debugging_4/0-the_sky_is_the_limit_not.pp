1c68fd67c4e925d6bedfe43527426dbb0bc5c0b3
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-06-11 09:04:26 +0000

    wed debbuging #4 update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,13 @@
-# Update the soft ulimit in the nginx default configuration file using sed
+# Update the soft ulimit in the Nginx default configuration file to 4096 using sed
+# This is necessary to allow Nginx to handle a higher number of file descriptors,
+# which helps to avoid "Too many open files" errors under high load.
 exec { 'update_nginx_default_ulimit':
   command => "/bin/sed -i 's/15/4096/' /etc/default/nginx",
   path    => '/usr/local/bin/:/bin/',
 }
 
-# Restart nginx service if the configuration file has been updated
+# Restart the Nginx service to apply the updated configuration
+# This ensures that the changes to the soft ulimit take effect immediately.
 exec { 'nginx-restart':
   command => '/etc/init.d/nginx restart',
   path    => '/etc/init.d/',
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
-# Define the ulimit command
-
-# Define the path to the ulimit script
+# Update the hard ulimit for the 'holberton' user in the limits.conf file
+# This is necessary to allow the 'holberton' user to have a higher maximum number of file descriptors.
 exec { 'add-user-hard-limit':
   command => "/bin/sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin',
 }
 
-# Define the path to the ulimit script
+# Update the soft ulimit for the 'holberton' user in the limits.conf file
+# This is necessary to allow the 'holberton' user to have a higher initial number of file descriptors.
 exec { 'add-user-soft-limit':
   command => "/bin/sed -i '/^holberton soft/s/4/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin',
