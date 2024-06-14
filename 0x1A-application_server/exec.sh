81973ae4d647ee642bad230c28564b6d619056ac
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-06-14 03:34:38 +0000

    made a bash script

---
+++ 0x1A-application_server/exec.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# This script sets up the environment and configures the server
+
+# Update and install necessary packages
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo apt-get install -y python3-pip
+pip3 install flask
+pip3 install sqlalchemy
+sudo apt install -y net-tools
+sudo apt install -y gunicorn
+sudo apt install -y tmux
+
+# Remove any previous versions of the repository and clone the new ones
+rm -rf AirBnB_clone_v*
+git clone https://github.com/G33GII/AirBnB_clone_v2.git
+git clone https://github.com/G33GII/AirBnB_clone_v3.git
+git clone https://github.com/G33GII/AirBnB_clone_v4.git
+
+# Checkout the app-server branch in the repositories
+cd AirBnB_clone_v2
+git checkout app-server
+cd ../AirBnB_clone_v3
+git checkout app-server
+cd ../AirBnB_clone_v4
+git checkout app-server
+cd ..
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared /var/www/html
+
+# Nginx configuration
+NGINX_CONFIG="/etc/nginx/sites-enabled/default"
+
+sudo tee "$NGINX_CONFIG" > /dev/null <<EOF
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    add_header X-Served-By \$hostname;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+
+    location /static/ {
+        alias /home/ubuntu/AirBnB_clone_v4/web_dynamic/static/;
+        try_files \$uri \$uri/ =404;
+    }
+
+    location / {
+        proxy_pass http://0.0.0.0:5003;
+    }
+
+    location /airbnb-onepage {
+        proxy_pass http://0.0.0.0:5000/airbnb-onepage;
+    }
+
+    location ~ ^/airbnb-dynamic/number_odd_or_even/([0-9]+)$ {
+        proxy_pass http://0.0.0.0:5001/number_odd_or_even/\$1;
+    }
+
+    location /api/ {
+        proxy_pass http://0.0.0.0:5002;
+    }
+
+    if (-f \$request_filename) {
+        rewrite ^ https://th3-gr00t.tk/ permanent;
+    }
+
+    error_page 404 /error_404.html;
+    location = /error_404.html {
+        internal;
+    }
+}
+EOF
+
+# Restart nginx to apply the new configuration
+sudo systemctl restart nginx
+
+# Start the Flask applications using gunicorn in tmux sessions
+cd /home/ubuntu/AirBnB_clone_v3
+tmux new-session -d 'gunicorn --bind 0.0.0.0:5002 api.v1.app:app'
+
+cd /home/ubuntu/AirBnB_clone_v4
+tmux new-session -d 'gunicorn --bind 0.0.0.0:5003 web_dynamic.app:app'
+
+cd /home/ubuntu/AirBnB_clone_v2
+tmux new-session -d 'gunicorn --bind 0.0.0.0:5000 web_flask.0-hello_route:app'
